5121e81bb3ea5e1756fa754f13b7810fe94c8e0b
Scott Roszel <57762591+sdroszel@users.noreply.github.com>
2024-08-23 17:28:45 -0400

    init

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+Ž
+^springbootdemo/src/main/java/com/springboot/demo/springbootdemo/SpringbootdemoApplication.java,1\1\11727faf942725a62db234a7779622487860515c
---
+++ springbootdemo/src/main/java/com/springboot/demo/springbootdemo/rest/FunRestController.java
@@ -0,0 +1,30 @@
+package com.springboot.demo.springbootdemo.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Daily Workout";
+    }
+
+    @GetMapping("/fortune")
+    public String getFortune() {
+        return "Outlook not good.";
+    }
+}
